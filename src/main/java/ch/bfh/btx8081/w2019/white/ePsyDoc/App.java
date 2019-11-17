e3ddcf77ea1cd10ad866a53b0a2823fdab4aac8f
selvs1 <42969019+selvs1@users.noreply.github.com>
2019-11-17 19:34:12 +0100

    fucking commints !!!!!

---
+++ src/main/java/ch/bfh/btx8081/w2019/white/ePsyDoc/App.java
@@ -0,0 +1,13 @@
+package ch.bfh.btx8081.w2019.white.ePsyDoc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

e2ac928ce1a7d6c535a65b4f566cb536cd27ac9c
selvs1 <42969019+selvs1@users.noreply.github.com>
2019-11-22 14:36:44 +0100

    Notwendige Classen vom Template kopieren

---
+++ src/main/java/ch/bfh/btx8081/w2019/white/ePsyDoc/MainView.java
@@ -0,0 +1,19 @@
+/*
+package ch.bfh.btx8081.w2019.white.ePsyDoc;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+
+@Route("dini")
+public class MainView extends VerticalLayout {
+    public MainView() {
+// Create the model and the Vaadin view implementation
+        CalculatorModel model = new CalculatorModel();
+        CalculatorViewImpl view = new CalculatorViewImpl();
+// The presenter connects the model and view
+        new CalculatorPresenter(model, view);
+// The view implementation is a Vaadin component
+        add(view);
+    }
+}*/

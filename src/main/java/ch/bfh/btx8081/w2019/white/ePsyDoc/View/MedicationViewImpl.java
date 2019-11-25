9ba3dc8f4c4fbc73d890694cebe1b26b1813dabd
milem2 <50821396+milem2@users.noreply.github.com>
2019-11-25 18:59:27 +0100

    Revert "1.7 MedicationView"
    This reverts commit b6ec327a520220406861113d4ceb78d79f6c5acd.

--- src/main/java/ch/bfh/btx8081/w2019/white/ePsyDoc/View/MedicationViewImpl.java
+++ src/main/java/ch/bfh/btx8081/w2019/white/ePsyDoc/View/MedicationViewImpl.java
@@ -3,17 +3,14 @@ package ch.bfh.btx8081.w2019.white.ePsyDoc.View;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H3;
-import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.listbox.ListBox;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -22,31 +19,9 @@ import ch.bfh.btx8081.w2019.white.ePsyDoc.MainView;
 @Route("Medication")
 @PageTitle("Medication")
 public class MedicationViewImpl extends MainLayoutView implements MedicationView{
-
-	VerticalLayout layout1 = new VerticalLayout();
-	HorizontalLayout layout2 = new HorizontalLayout();
-	TextField textfieldWirkstoff = new TextField("Wirkstoff");
-	TextField textfieldHandelsname = new TextField("Handelsname");
-	TextField textfieldStärke = new TextField("Stärke");
-	TextField textfieldForm = new TextField("Form");
-	TextField textfieldMorgen = new TextField("morgen");
-	TextField textfieldMittag = new TextField("mittag");
-	TextField textfieldAbend = new TextField("abend");
-	TextField textfieldNacht = new TextField("nacht");
-	TextField textfieldEinheit = new TextField("Einheit");
-	TextField textfieldHinweis = new TextField("Hinweis");
-	TextField textfieldGrund = new TextField("Grund");
-	Grid<Object> grid = new Grid<Object>();
 	
 	public MedicationViewImpl() {
-		
-		layout2.add(textfieldWirkstoff, textfieldHandelsname, textfieldStärke,
-				textfieldForm, textfieldMorgen, textfieldMittag, textfieldAbend,
-				textfieldNacht, textfieldEinheit, textfieldHinweis, textfieldGrund);
-		
-		layout1.add(layout2);
-		layout1.add(grid);
-		super.content.add(layout1);
+		super.content.add(new Button("MedicationViewImpl"));
 		
 	}
 	

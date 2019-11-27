5a369a8a13a6ed973b9fa12b4ba0134662719737
Alain <12994704+nippa1@users.noreply.github.com>
2019-11-27 19:00:33 +0100

    update

--- src/main/java/ch/bfh/btx8081/w2019/white/ePsyDoc/View/AppointmentViewImpl.java
+++ src/main/java/ch/bfh/btx8081/w2019/white/ePsyDoc/View/AppointmentViewImpl.java
@@ -1,6 +1,5 @@
 package ch.bfh.btx8081.w2019.white.ePsyDoc.View;
 
-
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -22,48 +21,43 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 import ch.bfh.btx8081.w2019.white.ePsyDoc.Model.Drug;
+import ch.bfh.btx8081.w2019.white.ePsyDoc.Model.MedicationPlan;
+import ch.bfh.btx8081.w2019.white.ePsyDoc.Model.PatientCase;
 import ch.bfh.btx8081.w2019.white.ePsyDoc.Model.PatientModel;
 
 @Route("Appointment")
 @PageTitle("Appointment")
 public class AppointmentViewImpl extends MainLayoutView implements MedicationView {
-private LocalDate date = LocalDate.now();
+	private LocalDate date = LocalDate.now();
+	private VerticalLayout root = new VerticalLayout();
+	private H1 title = new H1("Today's Patient");
+	private DatePicker datePicker = new DatePicker();
+	private Grid<PatientModel> patient = new Grid<>(PatientModel.class);
+	private List<PatientModel> personList = new ArrayList<>();
+	private Grid<PatientCase> patientCase = new Grid<>(PatientCase.class);
+	private List<PatientCase> patientCaseList = new ArrayList<>();
+
 	public AppointmentViewImpl() {
-		
-		
-
-		VerticalLayout root = new VerticalLayout();
-		H1 title = new H1("Today's Patient");
-		DatePicker datePicker = new DatePicker();
 		datePicker.setValue(date);
+		datePicker.addValueChangeListener(e -> date = datePicker.getValue());
+		patient.setItems(personList);
+		patient.removeColumnByKey("patientID");
+		patient.setColumns("firstname", "lastname", "date", "gender");
+		patientCase.setColumns("fid");
+		patientCase.setVisible(false);
+		patientCase.setItems(patientCaseList);
+		patient.addItemClickListener(e -> patientCase.setVisible(true));
 
-				datePicker.addValueChangeListener(
-		    e-> date = datePicker.getValue());
-			List<PatientModel> personList = new ArrayList<>();
+		personList.add(new PatientModel(1111, "Marley", "bla", "dsf", "sefs", "sdf", "cdsf"));
+		personList.add(new PatientModel(2222, "Muster", "blo", "dsf", "sefs", "sdf", "cdsf"));
+		personList.add(new PatientModel(3333, "Hauds", "blu", "dsf", "sefs", "sdf", "cdsf"));
 
-				personList.add(new PatientModel(1111, "Marley", "bla","dsf","sefs","sdf","cdsf"));
-				personList.add(new PatientModel(2222, "Muster", "blo","dsf","sefs","sdf","cdsf"));
-				personList.add(new PatientModel(3333, "Hauds", "blu","dsf","sefs","sdf","cdsf"));
-			
+		patientCaseList.add(new PatientCase("F124134", personList, personList, null));
+		patientCaseList.add(new PatientCase("F13423234", personList, personList, null));
+		patientCaseList.add(new PatientCase("F898767", personList, personList, null));
 
-				Grid<PatientModel> patient = new Grid<>(PatientModel.class);
-				patient.setItems(personList);
-
-				patient.removeColumnByKey("patientID");
-				patient.setColumns("firstname", "lastname", "date","gender");
-				
-				Grid<PatientModel> patientCase = new Grid<>(PatientModel.class);
-				patientCase.setItems(personList);
-
-				patientCase.removeColumnByKey("patientID");
-
-				//patientCase.setColumns("vorname", "name", "date","geschlecht");
-		
-		root.add(title, datePicker, patient);
-		content.add(root);
-		
+		root.add(title, datePicker, patient, patientCase);
+		super.content.add(root);
 	}
 
-
-	
 }

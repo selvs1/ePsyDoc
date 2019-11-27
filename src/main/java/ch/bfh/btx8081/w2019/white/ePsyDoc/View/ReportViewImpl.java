180f7162689448d4b4b21fcf4844727c8a16f6c1
Alain <12994704+nippa1@users.noreply.github.com>
2019-11-27 18:42:06 +0100

    Report View finished

--- src/main/java/ch/bfh/btx8081/w2019/white/ePsyDoc/View/ReportViewImpl.java
+++ src/main/java/ch/bfh/btx8081/w2019/white/ePsyDoc/View/ReportViewImpl.java
@@ -2,6 +2,9 @@ package ch.bfh.btx8081.w2019.white.ePsyDoc.View;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.richtexteditor.RichTextEditor;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -10,11 +13,26 @@ import com.vaadin.flow.router.Route;
 public class ReportViewImpl extends MainLayoutView implements MedicationView {
 
 	public ReportViewImpl() {
-		
-		
 		VerticalLayout root = new VerticalLayout();
+		MedicationViewImpl medicationView = new MedicationViewImpl();
+		medicationView.setWidth("100%");
 		
-		root.add(new MedicationViewImpl());
+		RichTextEditor consultation = new RichTextEditor("Consultation");
+		consultation.setWidth("100%");
+		
+		Button newB = new Button("New");
+		Button saveB = new Button("Save");
+		Button deleteB = new Button("Delete");
+
+		Tab tab1 = new Tab("C 2019.08.10");
+		Tab tab2 = new Tab("C 2019.10.04");
+		Tab tab3 = new Tab("C 2019.11.22");
+
+		Tabs tabs = new Tabs(tab1, tab2, tab3);
+		tabs.setFlexGrowForEnclosedTabs(1);
+		tabs.setWidth("100%");
+
+		root.add(tabs,newB,consultation,medicationView,saveB,deleteB);
 		content.add(root);
 	}
 }

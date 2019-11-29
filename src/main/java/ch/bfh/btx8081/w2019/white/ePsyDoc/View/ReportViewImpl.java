be47d0d2f1c49214ec4a695e10b4a91a5785de17
Alain <12994704+nippa1@users.noreply.github.com>
2019-11-29 16:03:46 +0100

    Update ReportViewImpl.java

--- src/main/java/ch/bfh/btx8081/w2019/white/ePsyDoc/View/ReportViewImpl.java
+++ src/main/java/ch/bfh/btx8081/w2019/white/ePsyDoc/View/ReportViewImpl.java
@@ -1,10 +1,12 @@
 package ch.bfh.btx8081.w2019.white.ePsyDoc.View;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.richtexteditor.RichTextEditor;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
+import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -14,14 +16,14 @@ public class ReportViewImpl extends MainLayoutView implements MedicationView {
 	private static final long serialVersionUID = 1L;
 	private VerticalLayout root = new VerticalLayout();
 	private MedicationViewImpl medicationView = new MedicationViewImpl();
-	private Button newB = new Button("New");
-	private Button saveB = new Button("Save");
-	private Button deleteB = new Button("Delete");
+	private Button newB = new Button(new Icon(VaadinIcon.FILE_ADD));
+	private Button saveB = new Button(new Icon(VaadinIcon.CHECK));
+	private Button deleteB = new Button(new Icon(VaadinIcon.FILE_REMOVE));
 	private Tab tab1 = new Tab("C 2019.08.10");
 	private Tab tab2 = new Tab("C 2019.10.04");
 	private Tab tab3 = new Tab("C 2019.11.22");
 	private Tabs tabs = new Tabs(tab1, tab2, tab3);
-	private RichTextEditor consultation = new RichTextEditor("Consultation");
+	private TextArea consultation = new TextArea("Consultation");
 
 	public ReportViewImpl() {
 		// medicationView settings

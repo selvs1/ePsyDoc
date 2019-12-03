999a0a4f89792cdabdda9188754498eccc1678e9
alainnippel <alainnippel@users.noreply.github.com>
2019-12-03 10:23:17 +0100

    ReportView update

--- src/main/java/ch/bfh/btx8081/w2019/white/ePsyDoc/View/ReportViewImpl.java
+++ src/main/java/ch/bfh/btx8081/w2019/white/ePsyDoc/View/ReportViewImpl.java
@@ -1,12 +1,16 @@
 package ch.bfh.btx8081.w2019.white.ePsyDoc.View;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
@@ -17,8 +21,22 @@ public class ReportViewImpl extends MainLayoutView implements ReportView {
 	private VerticalLayout root = new VerticalLayout();
 	private MedicationPlanViewImpl medicationView = new MedicationPlanViewImpl();
 	private Button newB = new Button(new Icon(VaadinIcon.FILE_ADD));
-	private Button saveB = new Button(new Icon(VaadinIcon.CHECK));
 	private Button deleteB = new Button(new Icon(VaadinIcon.FILE_REMOVE));
+	private Button addB = new Button(new Icon(VaadinIcon.PLUS));
+	private TextField diagnoseT = new TextField();
+	private Grid diagnoseG = new Grid();
+	private VerticalLayout information = new VerticalLayout();
+	private HorizontalLayout patientCaseLayout = new HorizontalLayout();
+	private HorizontalLayout doctorLayout = new HorizontalLayout();
+	private HorizontalLayout patientLayout = new HorizontalLayout();
+	private Icon doctorI = new Icon(VaadinIcon.DOCTOR);
+	private Icon patientI = new Icon(VaadinIcon.USER);
+	private Icon patientCaseI = new Icon(VaadinIcon.CLIPBOARD_CROSS);
+	private Label patientCaseL = new Label("123");
+	private Label doctorPrenameL = new Label("Doctor");
+	private Label doctorSurnameL = new Label("D");
+	private Label patientPrenameL = new Label("Patient");
+	private Label patientSurnameL = new Label("P");
 	private Tab tab1 = new Tab("C 2019.08.10");
 	private Tab tab2 = new Tab("C 2019.10.04");
 	private Tab tab3 = new Tab("C 2019.11.22");
@@ -31,18 +49,28 @@ public class ReportViewImpl extends MainLayoutView implements ReportView {
 
 		// consultation editor settings
 		consultation.setWidth("100%");
-
+		
+		// set header informations
+		doctorLayout.add(doctorI,doctorPrenameL, doctorSurnameL);
+		patientLayout.add(patientI,patientPrenameL, patientSurnameL);
+		patientCaseLayout.add(patientCaseI, patientCaseL);
+		information.add(doctorLayout, patientLayout,patientCaseLayout);
+		
+		// set Grid header
+		// set diagnose TextField settings
+		diagnoseT.setLabel("Diagnose");
+		
 		// Tabs settings
 		tabs.setFlexGrowForEnclosedTabs(1);
 		tabs.setWidth("100%");
 
 		// Add to layout
-		root.add(tabs, newB, consultation, medicationView, saveB, deleteB);
+		root.add(tabs, newB,information, consultation,diagnoseT,addB, diagnoseG, medicationView, deleteB);
 		super.content.add(root);
 	}
 
 	@Override
 	public void addListener(ReportViewListener listener) {
-		
+
 	}
 }

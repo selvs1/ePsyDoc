6645ce75daa5b377dcae090fb490c2d19cfe2f6f
milem2 <50821396+milem2@users.noreply.github.com>
2019-12-04 10:01:42 +0100

    update

--- src/main/java/ch/bfh/btx8081/w2019/white/ePsyDoc/View/ReportView.java
+++ src/main/java/ch/bfh/btx8081/w2019/white/ePsyDoc/View/ReportView.java
@@ -7,37 +7,49 @@ import ch.bfh.btx8081.w2019.white.ePsyDoc.Model.PatientCase;
 import ch.bfh.btx8081.w2019.white.ePsyDoc.Model.PatientModel;
 
 public interface ReportView {
-	
-	interface ReportViewListener {
-		//All
-		void save(int intervall);
-		void clickSetFields(PatientCase patientcase);
-		
-		//Report 
-		void clickNewReport(Doctor doctor, PatientModel patient);
-		void clickNewPatientCase(int patientCaseID, int patientID);
-		void clickDeletePatientCase();
-		void deleteDiagnose(); 
-		
-		//Medication
-		void clickSetMedicationPlan(int patientcaseID, int patientID);
-		void clickDisableFields();
-		void clickAutofill();
-		void clickAddMedication(Medication medication);
-		void clickRemoveFromMedicationPlan(String brandname);
 
+	interface ReportViewListener {
+		// All
+		void save(int intervall);
+
+		void clickSetFields(PatientCase patientcase);
+
+		void getModel(PatientModel patientModel);
+
+		// Report
+		void clickNewReport(Doctor doctor, PatientModel patient);
+
+		void clickNewPatientCase(int patientCaseID, int patientID);
+
+		void clickDeletePatientCase();
+
+		void deleteDiagnose();
+
+		// Medication
+		void clickSetMedicationPlan(int patientcaseID, int patientID);
+
+		void clickDisableFields();
+
+		void clickAutofill();
+
+		void clickAddMedication(Medication medication);
+
+		void clickRemoveFromMedicationPlan(String brandname);
 
 	}
 
 	void addListener(ReportViewListener listener);
-	
-	//Report
+
+	// Report
 	void displayPatientCaseTabs(int patientCaseID);
+
 	void displayDoctorName(int doctorID);
+
 	void displayPatientName(int patientID);
+
 	void displayPatientCaseID(int patientCaseID);
-	
-	//Medication
+
+	// Medication
 	void displayComboBox(HospIndex hospIndex);
-	
+
 }

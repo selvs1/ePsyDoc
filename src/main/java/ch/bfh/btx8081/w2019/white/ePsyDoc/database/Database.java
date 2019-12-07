2c18d471eb2f33160c27af480c213740e7715b31
selvs1 <42969019+selvs1@users.noreply.github.com>
2019-12-07 17:00:40 +0100

    Database Klasse erstellen

--- src/main/java/ch/bfh/btx8081/w2019/white/ePsyDoc/database/Database.java
+++ src/main/java/ch/bfh/btx8081/w2019/white/ePsyDoc/database/Database.java
@@ -1,4 +1,35 @@
 package ch.bfh.btx8081.w2019.white.ePsyDoc.database;
 
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+/**
+ * This static class helps persisting entity models into database. A side effect is also reducing the same code block for each entity model through centralization.
+ *
+ * @author Sugeelan Selvasingham
+ */
+
 public class Database {
+
+    private static EntityManager em = createEntityManager();
+    private static final String PERSISTENCE_UNIT_NAME = "ePsyDoc";
+
+    /**
+     * Create EntityManager with basic predefined PU name.
+     * @return EntityManager objekt with predefined PU name e.g. ePsyDoc.
+     */
+    public static EntityManager createEntityManager() {
+        return Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME).createEntityManager();
+    }
+
+    /**
+     * Return the current previously created EntityManager object.
+     * @return EntityManager object.
+     */
+    public static EntityManager getCurrentEntityManager() {
+        return em;
+    }
+
+
 }

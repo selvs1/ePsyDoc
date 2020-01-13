12f64b0102627617c2b71dc3d53a06a65e2cca1e
selvs1 <42969019+selvs1@users.noreply.github.com>
2020-01-13 20:53:51 +0100

    UnitTests mit Selenium versuchen.. geht leider nicht.

---
+++ src/test/java/ch/bfh/btx8081/w2019/white/ePsyDoc/LoginTest.java
@@ -0,0 +1,56 @@
+package ch.bfh.btx8081.w2019.white.ePsyDoc;
+
+import ch.bfh.btx8081.w2019.white.ePsyDoc.exceptions.DoctorException;
+import ch.bfh.btx8081.w2019.white.ePsyDoc.model.LoginModel;
+import ch.bfh.btx8081.w2019.white.ePsyDoc.presenter.LoginPresenter;
+import ch.bfh.btx8081.w2019.white.ePsyDoc.view.LoginViewImpl;
+import com.vaadin.flow.server.VaadinSession;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class LoginTest {
+
+
+    /**
+     * Vaadin sucks.
+     *
+     * @throws InterruptedException
+     * @deprecated This methode is used as a template for tests with solenium but unfortunately it doesn't work yet.
+     */
+    @Test
+    public void openWithChrome() throws InterruptedException {
+//        System.setProperty("webdriver.chrome.driver", "C:\\pathto\\my\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "D:\\Users\\sugeelan\\Downloads\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+        System.out.println(driver.getCurrentUrl());
+        driver.get("localhost:8080");
+        Thread.sleep(2000);
+        driver.get("localhost:8080");
+        Thread.sleep(2000);
+
+        WebElement btn = driver.findElement(By.name("button"));
+        btn.submit();
+        System.out.println("done");
+        driver.findElement(By.id("input")).sendKeys("pund3");
+
+        Thread.sleep(500);
+
+//        Thread.sleep(1000);  // Let the user actually see something!
+//        WebElement searchBox = driver.findElement(By.name("q"));
+//        searchBox.sendKeys("ChromeDriver");
+//        searchBox.submit();
+//        Thread.sleep(5000);  // Let the user actually see something!
+        driver.quit();
+    }
+
+}
